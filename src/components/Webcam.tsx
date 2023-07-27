18d9ef71694865c5136572bf36a4c7d58556620f
Brian Cho <brian.cho@clearobject.com>
2023-07-26 23:15:26 -0400

    lower confidence

--- src/components/Webcam.tsx
+++ src/components/Webcam.tsx
@@ -181,7 +181,7 @@ export const Webcam = ({ workoutOption }) => {
         await poseLandmarker.setOptions({
           runningMode: "VIDEO",
           minPoseDetectionConfidence: 0.8,
-          minPosePresenceConfidence: 0.9,
+          minPosePresenceConfidence: 0.8,
         });
         handleResize();
         initialize = false;
