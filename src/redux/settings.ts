773b9f3eb80f0626e96e69649d29ff827fee3ceb
Brian Cho <brian.cho@clearobject.com>
2023-08-08 23:20:06 -0400

    comment out

--- src/redux/settings.ts
+++ src/redux/settings.ts
@@ -1,31 +1,33 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+// import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-  angleUp: null,
-  angleDown: null,
-  thresholdTime: null,
-};
+// const initialState = {
+//   angleUp: null,
+//   angleDown: null,
+//   thresholdTime: null,
+// };
 
-const settingsSlice = createSlice({
-  name: "settings",
-  initialState,
-  reducers: {
-    setSettings: () => {
-      console.log("test");
-    },
-    getSettings: () => {
-      console.log("getSettings");
-    },
-  },
-});
+// const settingsSlice = createSlice({
+//   name: "settings",
+//   initialState,
+//   reducers: {
+//     setSettings: () => {
+//       console.log("test");
+//     },
+//     getSettings: () => {
+//       console.log("getSettings");
+//     },
+//   },
+// });
 
-export const { setSettings, getSettings } = settingsSlice.actions;
+// export const { setSettings, getSettings } = settingsSlice.actions;
 
-const store = configureStore({
-  reducer: settingsSlice.reducer,
-});
+// const store = configureStore({
+//   reducer: settingsSlice.reducer,
+// });
 
-store.subscribe(() => console.log(store.getState()));
+// store.subscribe(() => console.log(store.getState()));
 
-store.dispatch(setSettings());
-store.dispatch(getSettings());
+// store.dispatch(setSettings());
+// store.dispatch(getSettings());
+
+// https://redux.js.org/introduction/getting-started
